9c43d98f9d57ad82792fcf17a9015930fad68b5e
Git-hub-613 <hubgit473@gmail.com>
2025-06-25 08:17:47 +0545

    node.js completed courses

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,6 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
  const cloudinaryConnect = ()=>{
-    console.log(process.env.CLOUDINARY_CLOUD_NAME)
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_CLOUD_API_KEY, 
